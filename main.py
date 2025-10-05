2b02ae8b40dc6b078f2d844532178c0fdce228da
Kylobe <traedonharris29@gmail.com>
2025-10-04 21:17:20 -0600

    King Moves implemented

--- main.py
+++ main.py
@@ -2,12 +2,9 @@ from classes import Board
 
 def main():
     my_board = Board()
-    my_board.set_square(4, 4, "queen", "BLACK")
+    my_board.set_square(6, 4, "king", "BLACK")
     print(my_board)
-    print(my_board.get_square(4, 4).get_legal_moves())
+    print(my_board.get_square(6, 4).get_legal_moves())
 
 if __name__ == "__main__":
     main()
-
-
-
